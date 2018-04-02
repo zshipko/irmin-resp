f59822ed0d84bc43acaf5a0524c2ade20e2f2820
Zach Shipko <zachshipko@gmail.com>
2018-04-01 20:46:50 -0700

    Start integrating cmdliner

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,31 @@
 open Lwt.Infix
 open Irmin_server
+open Cmdliner
 
 module Store = Irmin_unix.Git.FS.KV(Irmin.Contents.String)
 module Server = Make(Store)
 
-let main =
-  let cfg = Irmin_git.config "/tmp/irmin" in
-  Store.Repo.v cfg >>= fun repo ->
-  Server.create (`TCP (`Port 1234)) repo >>= fun server ->
+let main addr port root =
+  let (module Server): (module S) = if false then (module Make(Irmin_unix.Git.Mem.KV(Irmin.Contents.String))) else (module (Make(Irmin_unix.Git.FS.KV(Irmin.Contents.String)))) in
+  let cfg = Irmin_git.config root in
+  Server.Store.Repo.v cfg >>= fun repo ->
+  Server.create ~host:addr (`TCP (`Port port)) repo >>= fun server ->
   Server.run server
 
-let _ = Lwt_main.run main
+let server addr port root = Lwt_main.run (main addr port root)
 
+let addr =
+  let doc = "Address to listen on" in
+  Arg.(value & opt string "127.0.0.1" & info ["a"; "address"] ~doc ~docv:"ADDR")
+
+let port =
+  let doc = "Port to listen on" in
+  Arg.(value & opt int 6978 & info ["p"; "port"] ~doc ~docv:"PORT")
+
+let root =
+  let doc = "Database root path" in
+  Arg.(value & opt string "/tmp/irmin" & info ["r"; "root"] ~doc ~docv:"ROOT")
+
+let server_t = Term.(const server $ addr $ port $ root)
+let cmd =   Term.info "irmin-server" ~version:"v0.1" ~exits:Term.default_exits
+let () = Term.exit @@ Term.eval (server_t, cmd)
